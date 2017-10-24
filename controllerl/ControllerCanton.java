dbc468c1e8f2120b6426c22a777ad2f3bdbb1c04
Bertil T <bertilt4@gmail.com>
2017-10-24 18:15:08 -0500

    Revert "Update ControllerCanton.java"
    This reverts commit 2606d4bd618b14a3b3cbc200526526d713b915ba.

--- controllerl/ControllerCanton.java
+++ controllerl/ControllerCanton.java
@@ -22,8 +22,6 @@ public class ControllerCanton {
 	private List<Distgeografica1> listadoi;
 //cambiado editado
 //otra vez ;lo he cambiado
-	
-	//ahora lo hemos cambiado desde web por que estaba mal 
 	@EJB
 	private ManagerCanton manager;
 	@EJB
